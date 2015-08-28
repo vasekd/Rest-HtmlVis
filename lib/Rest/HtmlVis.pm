b6ee51ea743a17c61e107e83f40c5c1256d7de95
Václav Dovrtěl <vaclav.dovrtel@gmail.com>
2015-08-28 16:25:27 +0200

    0.06

--- lib/Rest/HtmlVis.pm
+++ lib/Rest/HtmlVis.pm
@@ -4,7 +4,7 @@ use 5.008_005;
 use strict;
 use warnings FATAL => 'all';
 
-our $VERSION = '0.05'; # Set automatically by milla
+our $VERSION = '0.06'; # Set automatically by milla
 
 my $based = {
 	'default.base' => 'Rest::HtmlVis::Base',
--- lib/Rest/HtmlVis/Base.pm
+++ lib/Rest/HtmlVis/Base.pm
@@ -16,7 +16,7 @@ Version 0.01
 
 =cut
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 sub setStruct {
 	my ($self, $key, $struct, $env) = @_;
